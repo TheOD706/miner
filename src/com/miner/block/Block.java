579f2cd5234936519557a52e3a66eb5539d4f987
Oleg <Oleg@NOTE>
2016-12-25 15:34:13 +0200

    show header

--- src/com/miner/block/Block.java
+++ src/com/miner/block/Block.java
@@ -132,11 +132,11 @@ public class Block {
 		byte[] t = swap(ntime);
 		byte[] b = swap(nbits);
 		byte[] header_prefix_bin = concatByteArray(v, concatByteArray(p, concatByteArray(merkle, concatByteArray(t, b))));
+		System.out.println(hexlify(header_prefix_bin));
 		byte[] nonce_bin = swap(nonce2);
 		String hash = hexlify(swap(sha256d(concatByteArray(header_prefix_bin, nonce_bin))));
 		System.out.println(hash);
 		System.out.println(Target());
-		System.out.println(hash.compareTo(Target()) < 0);
 		return hash.compareTo(Target()) < 0;
 	}
 
